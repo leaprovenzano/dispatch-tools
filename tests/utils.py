d133cb966b9def4030790a9751ea82762476c53c
lea <leaprovenzano@gmail.com>
2019-12-15 12:42:42 +0000

    add not_raises in test utils

--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,6 @@
+import re
+from typing import Optional
+from contextlib import contextmanager
 import pytest
 
 
@@ -7,3 +10,16 @@ def dependency(f=None, depends=None):
 
     name = f.__name__.strip('test_')
     return pytest.mark.dependency(name=name, depends=depends)(f)
+
+
+@contextmanager
+def not_raises(exception: Exception, match: Optional[str] = None):
+    try:
+        yield
+    except exception as exc:
+        if match is not None:
+            m = re.match(match, str(exc))
+            if m is None:
+                raise
+        else:
+            raise pytest.fail("DID RAISE {0}".format(exception))
